0157ee604eab424de364a1b821385b38e93c99b8
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-02-22 17:55:20 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TableKitBinaries
+
+A description of this package.
---
+++ Sources/TableKitBinaries/TableKitBinaries.swift
@@ -0,0 +1,6 @@
+public struct TableKitBinaries {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
